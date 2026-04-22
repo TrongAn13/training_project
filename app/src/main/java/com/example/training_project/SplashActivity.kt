e785990a486e840f2635143991beb6ad61efc94e
CodeWithAnn <tronganfwork@gmail.com>
2026-04-22 13:17:37 +0700

    update

--- app/src/main/java/com/example/training_project/SplashActivity.kt
+++ app/src/main/java/com/example/training_project/SplashActivity.kt
@@ -10,17 +10,17 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        val pref = PreferenceManager(this)
-        Handler(Looper.getMainLooper()).postDelayed({
-            if(pref.isLoggedIn()){
-                val intent = Intent(this, HomeActivity::class.java)
-                startActivity(intent)
-            }
-            else {
-                val intent = Intent(this, LoginActivity::class.java)
-                startActivity(intent)
-            }
-            finish()
-        }, 2000)
+
+        val pref = PreferenceManager.getInstance(this)
+
+        val targetActivity = if (pref.isLoggedIn()) {
+            HomeActivity::class.java
+        } else {
+            LoginActivity::class.java
+        }
+
+        startActivity(Intent(this, targetActivity))
+
+        finish()
     }
 }
